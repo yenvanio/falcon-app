0ba2fa8a65be783d81996658295aaff32f42dede
Shivanouyen Yoganathan <shivanouyen.yoganathan@ontariotechu.net>
2024-06-24 19:13:03 -0400

    init

---
+++ pages/about-us.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function AboutUs() {
+    return (
+        <>
+            <h1>About Us</h1>
+            <h2>
+                <Link href="/">Back to home</Link>
+            </h2>
+        </>
+    );
+}
---
+++ pages/search.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+
+export default function Search() {
+    return (
+        <>
+            <h1 className={styles.title}>
+                Search
+            </h1>
+
+
+        </>
+    );
+}
