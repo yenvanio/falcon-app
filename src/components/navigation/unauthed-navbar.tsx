98877bf7ec3374902a10a2f14a77f949efdb8b81
Shivanouyen Yoganathan <shivanouyen.yoganathan@ontariotechu.net>
2024-07-06 11:40:48 -0400

    test

--- src/components/navigation/unauthed-navbar.tsx
+++ src/components/navigation/unauthed-navbar.tsx
@@ -11,18 +11,16 @@ export default async function UnauthedNavbar() {
         <header className="bg-slate-900 text-white px-4 py-6 flex items-center justify-between">
             <nav className="flex items-center gap-4">
                 <Link href="#" className="flex items-center justify-center" prefetch={false}>
-                    {/*<PlaneIcon className="h-6 w-6" />*/}
                     <img
                         src="/falcon.svg"
                         alt="falcon"
                         className="h-6 w-6"
                     />
-                    {/*<AppIcon className="h-6 w-6 text-yellow-400"/>*/}
                     <span className="font-semibold ml-2">Falcon</span>
                 </Link>
             </nav>
             <div className="float-right">
-                {/*<LoginDialog />*/}
+                <LoginDialog />
             </div>
         </header>
     )
